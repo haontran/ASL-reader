1613b1ab3223fc50f6c27d121b94b715011ab702
haontran <164290050+haontran@users.noreply.github.com>
2026-04-24 10:55:22 -0500

    Added data collection for hand landmarks
    - Implemented data collection script using MediaPipe HandLandmarker
    - Captures hand landmarks from webcam
    - Convert 21 hand landmarks into flattened feature vectors
    - Add keyboard input for dynamic labeling
    - Implement cooldown to prevent duplicate samples
    - Track sample counts during collection
    
    Next steps:
    - Learn the ASL alphabet
    - Collect alphabet data to train the model

--- src/collect_hand_data.py
+++ src/collect_hand_data.py
@@ -1,12 +1,19 @@
 import time
 import cv2
+import csv
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-
+DATA_FILE = "data/hand_landmarks.csv"
 MODEL_PATH = "models/hand_landmarker.task"
 
+file = open(DATA_FILE, mode="a", newline="")
+writer = csv.writer(file)
+
+last_save_time = 0
+SAVE_DELAY = 1.0
+sample_counts = {}
 
 def draw_landmarks(frame, hand_landmarks):
     height, width, _ = frame.shape
@@ -32,6 +39,14 @@ def draw_landmarks(frame, hand_landmarks):
 
     for point in points:
         cv2.circle(frame, point, 5, (0, 255, 0), -1)
+        
+def landmarks_to_row(hand_landmarks):
+    row = []
+    
+    for landmark in hand_landmarks:
+        row.extend([landmark.x, landmark.y, landmark.z])
+    
+    return row
 
 base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
 
@@ -62,24 +77,33 @@ while True:
     timestamp_ms = int(time.time() * 1000)
     result = landmarker.detect_for_video(mp_image, timestamp_ms)
 
+    current_hand_landmarks = None
+
     if result.hand_landmarks:
+        current_hand_landmarks = result.hand_landmarks[0]
+
         for hand_landmarks in result.hand_landmarks:
             draw_landmarks(frame, hand_landmarks)
 
-            wrist = hand_landmarks[0]
-            index_tip = hand_landmarks[8]
-            thumb_tip = hand_landmarks[4]
-
-            print("Wrist:", wrist.x, wrist.y, wrist.z)
-            print("Index tip:", index_tip.x, index_tip.y, index_tip.z)
-            print("Thumb tip:", thumb_tip.x, thumb_tip.y, thumb_tip.z)
-            print("---")
-
     cv2.imshow("Hand Detection", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    key = cv2.waitKey(1) & 0xFF
+
+    current_time = time.time()
+
+    if current_hand_landmarks and 97 <= key <= 122:
+        if current_time - last_save_time > SAVE_DELAY:
+            label = chr(key).upper()
+            row = [label] + landmarks_to_row(current_hand_landmarks)
+            writer.writerow(row)
+            sample_counts[label] = sample_counts.get(label, 0) + 1
+            print(f"Saved sample for {label}. Count this run: {sample_counts[label]}")
+            last_save_time = current_time
+
+    if key == 27:
         break
 
 cap.release()
 landmarker.close()
 cv2.destroyAllWindows()
+file.close()
